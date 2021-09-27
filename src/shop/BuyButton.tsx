3642df71f3ea71dc3f318a5be23b9b07558f4306
SimonSchou <sje@it-minds.dk>
2021-09-27 12:53:40 +0200

    added buyItemFromShop mutation function to buyButton

--- src/shop/BuyButton.tsx
+++ src/shop/BuyButton.tsx
@@ -2,18 +2,34 @@ import React, { FC } from 'react';
 import ShopIcon from '@material-ui/icons/ShoppingCart';
 import { IconButton } from '@material-ui/core';
 import { useLocalData } from '../useLocalData';
+import gql from 'graphql-tag';
+import { useSWMutaion } from '../utils/useSWMutation';
+import { buyItemMutation, buyItemMutationVariables } from './__generated__/buyItemMutation';
+import { userItemQuery } from '../profile/UserItemList';
+import { GetAllItemsQuery } from './ShopRoot';
 
 type BuyButtonProps = { itemId: string | undefined };
+
+export const buyItemFromShop = gql`
+  mutation buyItemMutation($userId: ID!, $itemId: ID!) {
+    buyItem(input: { userId: $userId, itemId: $itemId }) {
+      money
+    }
+  }
+`;
+
 export const BuyButton: FC<BuyButtonProps> = ({ itemId }) => {
-  const localUser = useLocalData();
+  const [{ id }] = useLocalData();
+  const [buyItem, { data }] = useSWMutaion<buyItemMutation, buyItemMutationVariables>(buyItemFromShop, {
+    refetchQueries: [{ query: userItemQuery, variables: { id: id } }, { query: GetAllItemsQuery }],
+  });
+
+  if (id === undefined || itemId === undefined) {
+    return <div> something wong </div>;
+  }
 
   return (
-    <IconButton
-      onClick={() => {
-        //console.log(itemId)
-      }
-      }
-    >
+    <IconButton onClick={() => buyItem({ variables: { userId: id, itemId: itemId } })}>
       <ShopIcon />
     </IconButton>
   );
