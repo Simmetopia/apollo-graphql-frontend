064fd50c6df5110fb27204aff60c27c998bd743f
SimonSchou <sje@it-minds.dk>
2021-09-28 09:19:27 +0200

    Added login on enter press

--- src/auth/CreateUserForm.tsx
+++ src/auth/CreateUserForm.tsx
@@ -1,9 +1,30 @@
-import React, { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { LoginAsUser, CreateUserButton } from './SignupButtons';
 import { TextField, Grid } from '@material-ui/core';
+import { userLoginQuery, userLoginQueryVariables } from './__generated__/userLoginQuery';
+import { UserLoginQuery } from './SignupButtons';
+import { useSWLazyQuery } from '../utils/useSWLazyQuery';
+import { useLocalData } from '../useLocalData';
 
 export const CreateUserForm: FC = () => {
   const [username, setUsername] = useState('');
+  const [_, setData] = useLocalData();
+
+  const [userLoginQuery, { data }] = useSWLazyQuery<userLoginQuery, userLoginQueryVariables>(UserLoginQuery, {
+    variables: { username },
+  });
+
+  useEffect(() => {
+    if (!data) return;
+    setData({ name: data.GetUserByUsername?.username, id: data.GetUserByUsername?.id });
+  }, [data]);
+
+  const handleKeyPress = (e: { key: string }) => {
+    if (e.key === 'Enter') {
+      console.log(username);
+      userLoginQuery({ variables: { username } });
+    }
+  };
   return (
     <Grid container direction="column" spacing={2}>
       <TextField
@@ -12,6 +33,8 @@ export const CreateUserForm: FC = () => {
         color="secondary"
         placeholder={'username'}
         onChange={(e) => setUsername(e.target.value)}
+        autoFocus
+        onKeyDown={handleKeyPress}
       />
       <Grid item>
         <Grid container spacing={1}>
