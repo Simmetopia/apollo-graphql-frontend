c9abd765baa28d744b6fd50efcd683319b6d6dba
Alexander-Wodstrup <afw@it-minds.dk>
2021-09-29 09:05:01 +0200

    Tilføjede funktionalitet for at vise et item billede for hvert slagt card

--- src/profile/SingleItemCard.tsx
+++ src/profile/SingleItemCard.tsx
@@ -1,29 +1,71 @@
 import React, { FC } from 'react';
-import { Typography, Card } from '@material-ui/core';
+import { Typography, Card, CardMedia } from '@material-ui/core';
 import { getAllItemsQuery_GetAllItems } from '../shop/__generated__/getAllItemsQuery';
 import '../shop/Shop.css';
+import { getAllItemsInShopQuery_GetAllItemsInShop } from '../shop/__generated__/getAllItemsInShopQuery';
 
-const SingleItemCard: FC<{ item: getAllItemsQuery_GetAllItems | null }> = ({ children, item }) => {
+const SingleItemCard: FC<{ item: getAllItemsInShopQuery_GetAllItemsInShop | null }> = ({ children, item }) => {
   if (!item) {
     return <div> something wong </div>;
   }
+  var imageBackgroundColor: string = "#dfe6e9"
+  if(item.partDescription?.includes("uncommon")){
+    imageBackgroundColor = "#00b894"
+  } else if(item.partDescription?.includes("rare")) 
+  {
+    imageBackgroundColor = "#0984e3"
+  } else if(item.partDescription?.includes("epic")) 
+  {
+    imageBackgroundColor = "#6c5ce7"
+  } else if(item.partDescription?.includes("LEGENDARY")) 
+  {
+    imageBackgroundColor = "#fdcb6e"
+  }
 
   return (
-    <Card className="CardItem" style={{ backgroundColor: 'ThreeDDarkShadow' }}>
+    <div className="box">
+    <Card className="CardItem" style={{ backgroundColor: 'ThreeDDarkShadow' , height: '100%', border: '3px solid'}}>
+      <div className="center" style={{backgroundColor: imageBackgroundColor,  borderRadius: 5, border: '3px solid',}}>
+        <img src={item.url ?? ""} alt="Commando Switch" style={{objectFit: 'cover', height: 100}} />
+      </div>
+     <div style={{display: 'flex', gap: 5}}>
       <Typography color="primary">
-        <strong>Name: {item.partName}</strong>
+        Name:
       </Typography>
       <Typography color="primary">
-        <strong>Price: {item.price} coins</strong>
+        <strong>{item.partName}</strong>
+      </Typography>
+     </div>
+      <div style={{display: 'flex', gap: 5}}>
+      <Typography color="primary">
+        Price:
       </Typography>
       <Typography color="primary">
-        <strong>Part: {item.saberPart}</strong>
+        <strong>{item.price}</strong>
       </Typography>
       <Typography color="primary">
-        <strong>Description: {item.partDescription}</strong>
+        coins
       </Typography>
+      </div>
+      <div style={{display: 'flex', gap: 5}}>
+      <Typography color="primary">
+        Part:
+      </Typography>
+      <Typography color="primary">
+        <strong>{item.saberPart}</strong>
+      </Typography>
+      </div>
+      <div style={{display: 'flex', gap: 5}}>
+      <Typography color="primary">
+        Description: 
+      </Typography>
+      <Typography color="primary">
+        <strong>{item.partDescription}</strong>
+      </Typography>
+      </div>
       {children}
     </Card>
+    </div>
   );
 };
 export default SingleItemCard;
