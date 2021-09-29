5cdde3ee935c6ed4b61eb29e7c2fad63cec927ea
SimonBjermandKjaer <sbk@it-minds.dk>
2021-09-29 15:50:39 +0200

    filter Items in shop

--- src/profile/SingleItemCard.tsx
+++ src/profile/SingleItemCard.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react";
+import { ItemFilterQuery_filterItems } from "../shop/__generated__/ItemFilterQuery";
 import '../styles/ProfileStyle.css';
 import { SWLabelValue } from "./SWLabelValue";
-import { ItemDisplayQuery_displayItems } from "./__generated__/ItemDisplayQuery";
 
-const SingleItemCard: FC<{ item: ItemDisplayQuery_displayItems }> = ({ children, item }) => {
+const SingleItemCard: FC<{ item: ItemFilterQuery_filterItems }> = ({ children, item }) => {
 
     return (
-        <div className="rounded p-3 shadow w-96 flex flex-col gap-3" style={{ backgroundColor: "#616161", height: "100%" }}>
+        <div className="rounded p-3 shadow w-80 flex flex-col gap-3" style={{ backgroundColor: "#616161", height: "100%" }}>
             <SWLabelValue label="Name:" value={item.PartName!.name}></SWLabelValue>
             <SWLabelValue label="Price:" value={'' + item.price} ></SWLabelValue >
             <SWLabelValue label="Part:" value={item.SaberPart!.name}></SWLabelValue>
