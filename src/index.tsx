e7c4374e0af017b9bc96ac3fba25c5651aeec81b
rwlITMinds <rwl@it-minds.dk>
2021-10-14 15:33:50 +0200

    Removed /graphql from uri

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import { WebSocketLink } from '@apollo/client/link/ws';
 import { getMainDefinition } from '@apollo/client/utilities';
 
 const httpLink = new HttpLink({
-  uri: 'https://notacultbruh.herokuapp.com/graphql',
+  uri: 'https://notacultbruh.herokuapp.com',
 });
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
